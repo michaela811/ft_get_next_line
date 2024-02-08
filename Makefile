6fb78f9567466c42e01e3fe0e9ac4a51953de9b1
michaela811 <102808406+michaela811@users.noreply.github.com>
2024-02-08 10:47:38 +0000

    added FINAL

--- Makefile
+++ Makefile
@@ -1,26 +1,39 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: codespace <codespace@student.42.fr>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/02/06 12:32:20 by mmasarov          #+#    #+#              #
+#    Updated: 2024/02/08 10:36:49 by codespace        ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 CC = cc
 CFLAGS = -g -Wall -Wextra -Werror
-NAME = getnextline.a
+NAME = gnl
 HEADER = get_next_line.h
 
 SOURCES = get_next_line_utils.c \
 		get_next_line.c \
+		main.c		
 
 OBJECT = $(SOURCES:.c=.o)
 all: $(NAME)
 
-$(NAME): $(OBJECT)
-		ar rcs $@ $(OBJECT)
-
-%.o: %.c $(HEADER)
+%.o: %.c
 		$(CC) $(CFLAGS) -c $< -o $@
 
-.PHONY: clean fclean re all
+$(NAME): $(OBJECT)
+		$(CC) $(CFLAGS) $(OBJECT) -o $(NAME)
+		
+.PHONY:	clean fclean re all
 
 clean:
-		rm -rf $(OBJECT)
+		rm -f $(OBJECT)
 
-fclean:		clean
+fclean:	clean
 		rm -rf $(NAME)
 
-re: fclean $(NAME)
+re:	fclean all
--- main.c
+++ main.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 12:33:15 by mmasarov          #+#    #+#             */
+/*   Updated: 2024/02/08 10:47:19 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-/*int main()
+int main()
 {
     int     fd1;
     char    *line;
@@ -8,9 +20,10 @@
 
     i = 1;
     //line = NULL;
-    fd1 = open("tests/test1.txt", O_RDONLY);
+    fd1 = open("file_to_read.txt", O_RDONLY);
     line = get_next_line(fd1);
     printf("Line n.%d: %s", i, line);
+    i++; 
     while (line)
     {
         free(line);
@@ -20,70 +33,4 @@
     }
     close(fd1);
     return (0);
-}*/
-
-int	main(void)
-{
-	char	*result;
-	int		fd;
-	int		iterations;
-
-	fd = open("day2.txt", O_RDONLY);
-	//printf("FD is %d\n", fd);
-	iterations = 100;
-	int line_sum = 0;
-	char buffer[2];
-	int sum = 0;
-	char *number_first;
-	for (int i = 0; i < iterations; i++)
-	{
-		result = get_next_line(fd);
-		if (get_cubes(result) == 0)
-			line_sum = get_number(result);
-			printf("%d\n", line_sum);
-		//char number_last = get_last(result);
-		//buffer[0] = number_first;
-		//buffer[1] = number_last;
-		//buffer[2] = '\0';
-		//line_sum = atoi(number_first);
-		//printf("%d\n", sum);
-		//sum = sum + line_sum;
-		//printf("%d\n", sum);
-		free(result);
-	}
-	printf("=%d", sum);
-	close(fd);
-	//fd = open("tests/test1.txt", O_RDONLY);
-	//printf("FD is %d\n", fd);
-
-	//fd = open("tests/test1.txt", O_RDONLY);
-
-	// result = get_next_line(fd);
-	// printf("%s", result);
-	// while (result)
-	// {
-	// 	free(result);
-	// 	result = get_next_line(fd);
-	// 	printf("%s", result);
-	// }
-	//iterations = 2;
-	//for (int i = 0; i < iterations; i++)
-	//{
-	//	result = get_next_line(fd);
-	//	printf("|%d: %s", i + 1, result);
-	//	free(result);
-	//}
-	//close(fd);
-
-	//int fd1 = open("tests/test1.txt", O_RDONLY);
-	//iterations = 5;
-	//for (int i = 0; i < iterations; i++)
-	//{
-	//	result = get_next_line(fd);
-	//	printf("|%d: %s", i + 1, result);
-	//	free(result);
-	//}
-	//close(fd);
-    //close(fd1);
-	return (0);
 }
